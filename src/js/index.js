a2697996f7f02566b5ad134c83bdad01b04e04d1
khmilos <khmil.oleksandr32@gmail.com>
2020-09-12 12:12:08 +0300

    prettier & eslint

--- src/js/index.js
+++ src/js/index.js
@@ -3,34 +3,29 @@
 
 class A {
   constructor(a) {
-    console.log(a)
-    this.a = a
+    console.log(a);
+    this.a = a;
   }
 
-  foo() {
-    console.log(1123123);
-    let a = 1;
-    let b = 200;
-    console.log(a,b);
-  }
+  static foo() {}
 }
 
-console.log('hello world')
+console.log('hello world');
 
-console.log('Another hello world')
+console.log('Another hello world');
 
-new A(100).foo()
+new A(100).foo();
 
 function foo() {
-  let a = 100
-  console.log('hello world')
-  console.log(a)
+  const a = 100;
+  console.log('hello world');
+  console.log(a);
   console.log(
     (() => {
-      console.log(11)
-      return 0
-    })()
-  )
+      console.log(11);
+      return 0;
+    })(),
+  );
 }
 
-foo()
+foo();
