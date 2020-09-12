0d110e129334adb1762a662e72b9cf15015fc786
khmilos <khmil.oleksandr32@gmail.com>
2020-09-12 08:05:46 +0300

    prettier before

--- src/js/index.js
+++ src/js/index.js
@@ -4,28 +4,33 @@
 class A {
   constructor(a) {
     console.log(a)
-    this.a = a;
+    this.a = a
   }
 
   foo() {
     console.log(1123123);
+    let a = 1;
+    let b = 200;
+    console.log(a,b);
   }
 }
 
-console.log('hello world');
+console.log('hello world')
 
-console.log('Another hello world');
+console.log('Another hello world')
 
 new A(100).foo()
 
 function foo() {
-  let a = 100;
-  console.log('hello world');
-  console.log(a);
-  console.log((() => { 
-    console.log(11); 
-    return 0; 
-  })())
+  let a = 100
+  console.log('hello world')
+  console.log(a)
+  console.log(
+    (() => {
+      console.log(11)
+      return 0
+    })()
+  )
 }
 
-foo();
+foo()
